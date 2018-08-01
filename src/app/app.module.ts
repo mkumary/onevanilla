102781a6032725e623c66d711b78dd274f7b71e9
mkumary <manoj10690@gmail.com>
2018-08-01 23:43:07 +0530

    separated header and footer as components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,25 @@ import {routing} from './app.routing';
 
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { WhereToBuyComponent } from './where-to-buy/where-to-buy.component';
 import { UsingYourCardComponent } from './using-your-card/using-your-card.component';
 import { TryMyVanillaComponent } from './try-my-vanilla/try-my-vanilla.component';
 import { FaqComponent } from './faq/faq.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     HomeComponent,
     WhereToBuyComponent,
     UsingYourCardComponent,
     TryMyVanillaComponent,
-    FaqComponent
+    FaqComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
